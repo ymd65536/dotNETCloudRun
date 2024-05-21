92e2dbc8acf6aafaf4f590950535846e33a701b4
ymd65536 <ymd65536@gmail.com>
2024-05-22 00:47:07 +0900

    add: google auth

--- BlazorServerApp/Program.cs
+++ BlazorServerApp/Program.cs
@@ -2,12 +2,30 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using dotNETCloudRun.Data;
 
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddAuthentication(options => {
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+    }).AddCookie()
+    .AddGoogle(options => {
+    options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+    options.ClaimActions.MapJsonKey("urn:google:profile", "link");
+    options.ClaimActions.MapJsonKey("urn:google:image", "picture");
+});
+
+builder.Services.AddHttpClient();           // 注入@inject HttpClient Http
+builder.Services.AddHttpContextAccessor();  
 
 var app = builder.Build();
 
@@ -22,6 +40,10 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseCookiePolicy();
 
 app.UseRouting();
 
